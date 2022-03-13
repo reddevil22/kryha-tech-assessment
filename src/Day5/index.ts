e0f83774724af89935601841b9376c583ac2281a
Joel Fredericks <jk.fredericks@gmail.com>
2022-03-13 19:59:50 +0200

    Finished day 5 again lol

--- src/Day5/index.ts
+++ src/Day5/index.ts
@@ -4,15 +4,15 @@ import { join } from 'path';
 const calculateChange = (lineChange: string[]) => {
     let [xStart, yStart] = lineChange[0].split(',').map(value => Number(value));
     let [xEnd, yEnd] = lineChange[1].split(',').map(value => Number(value));
-    
+
     const change = {
         x: xEnd - xStart,
         y: yEnd - yStart,
     }
-    
+
     const xChanges = [];
-    if(xEnd - xStart !== 0 || xStart - xEnd !== 0) {
-        if(change.x > 0) {
+    if (xEnd - xStart !== 0 || xStart - xEnd !== 0) {
+        if (change.x > 0) {
             for (let index = 0; index <= change.x; index++) {
                 xChanges.push([xStart++, yStart])
             }
@@ -25,8 +25,8 @@ const calculateChange = (lineChange: string[]) => {
     }
 
     const yChanges = [];
-    if(yEnd - yStart !== 0 || yStart - yEnd !== 0) {
-        if(change.y > 0) {
+    if (yEnd - yStart !== 0 || yStart - yEnd !== 0) {
+        if (change.y > 0) {
             for (let index = 0; index <= change.y; index++) {
                 yChanges.push([xStart, yStart++])
             }
@@ -38,49 +38,38 @@ const calculateChange = (lineChange: string[]) => {
         }
     }
 
-    if(xChanges.length > 0) {
+    if (xChanges.length > 0) {
         return xChanges;
     }
-    else if(yChanges.length > 0) {
+    else if (yChanges.length > 0) {
         return yChanges;
     }
 }
 
 const input = readFileSync(join(__dirname, '..', 'vents.txt'), 'utf-8')
-.toString()
-.trim()
-.split('\n')
-.map((r) => r.trim())
-.filter(line => {
-    const segment = line.split('->').map(coord => coord.trim());
-    const [xStart, yStart] = segment[0].split(',').map(value => Number(value));
-    const [xEnd, yEnd] = segment[1].split(',').map(value => Number(value));
-    return (xStart === xEnd || yStart === yEnd);
-});
-
-console.log("🚀 ~ file: index.ts ~ line 16 ~ input", input)
+    .toString()
+    .trim()
+    .split('\n')
+    .map((r) => r.trim())
+    .filter(line => {
+        const segment = line.split('->').map(coord => coord.trim());
+        const [xStart, yStart] = segment[0].split(',').map(value => Number(value));
+        const [xEnd, yEnd] = segment[1].split(',').map(value => Number(value));
+        return (xStart === xEnd || yStart === yEnd);
+    });
 
 const changes = input.map(line => {
     return calculateChange(line.split('->').map(coord => coord.trim()))
 })
-console.log("🚀 ~ file: index.ts ~ line 66 ~ changes", changes.flat(1))
 
- const vents = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-]
-console.log("🚀 ~ file: index.ts ~ line 41 ~ vents", vents)
+//@ts-ignore
+const maxX = Math.max(...changes.map(x => x.map(a => a[0])).flat(2))
+//@ts-ignore
+const maxY = Math.max(...changes.map(x => x.map(a => a[0])).flat(2))
+const dynamicVents = new Array(maxX + 1).fill(0).map(() => new Array(maxY + 1).fill(0));
 
 changes.flat(1).forEach(change => {
-    if(change) vents[change[1]][change[0]]++
+    if (change) dynamicVents[change[1]][change[0]]++
 })
 
-console.log("🚀 ~ file: index.ts ~ line 41 ~ vents", vents)
+console.log("how many points do at least two lines overlap?: ", dynamicVents.flat(2).filter(point => point >= 2).length)
